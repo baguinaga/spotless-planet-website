8c232366e4f114d032c86f9eab66d2754a2ca3ba
Billy Aguinaga <aguinaga.b@gmail.com>
2025-01-28 00:20:23 -0500

    loading footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './utility/header/header.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
