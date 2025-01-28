420976c2a709d1c004bb518276a4db5619331a98
Billy Aguinaga <aguinaga.b@gmail.com>
2025-01-27 23:39:56 -0500

    wip footer comp

---
+++ src/app/utility/footer/footer.component.ts
@@ -0,0 +1,58 @@
+import {
+  Component,
+  AfterViewInit,
+  ElementRef,
+  Renderer2,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent implements AfterViewInit {
+  contactInfo = [
+    {
+      icon: 'icon__phone',
+      id: 'phone-number',
+      text: 'Telephone: ',
+      anchor: '555-555-5555',
+      href: 'tel:5555555555',
+    },
+    {
+      icon: 'icon__email',
+      id: 'email-address',
+      text: 'Email: ',
+      anchor: 'info@spotlessplanet.com',
+      href: 'mailto:info@spotlessplanet.com',
+    },
+    {
+      icon: 'icon__calendar',
+      id: 'business-hours',
+      text: 'Hours:',
+      anchor: '<br>Mon - Sat: 8:00 am - 6:00 pm,<br>Sun: Closed',
+      href: '#',
+    },
+  ];
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    const links =
+      this.elementRef.nativeElement.querySelectorAll('.obfuscated-link');
+    links.forEach((link) => {
+      const href = link.getAttribute('data-href');
+      if (href) {
+        this.renderer.setAttribute(link, 'href', href);
+        this.cd.detectChanges();
+      }
+    });
+  }
+}
