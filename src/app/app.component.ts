8c01c028f94787cbcc3788700458150207709507
Billy Aguinaga <aguinaga.b@gmail.com>
2025-01-27 23:34:24 -0500

    nav header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './utility/header/header.component';
+import { FooterComponent } from './utility/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'spotless-planet-website';
---
+++ src/app/utility/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  navLinks = [
+    {
+      text: 'Home',
+      href: 'home',
+      default: true,
+    },
+    {
+      text: 'About',
+      href: 'about',
+      default: false,
+    },
+    {
+      text: 'Services',
+      href: 'services',
+      default: false,
+    },
+    {
+      text: 'Contact',
+      href: 'contact',
+      default: false,
+    },
+  ];
+}
