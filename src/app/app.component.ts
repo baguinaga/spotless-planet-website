1a3d319c7c560212de11dce41b1e5e222d3869d7
Billy Aguinaga <aguinaga.b@gmail.com>
2025-01-30 02:13:33 -0500

    hero-banner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './navigation/header/header.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { HeroBannerComponent } from './sections/hero-banner/hero-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, HeroBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
