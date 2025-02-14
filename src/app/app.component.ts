db04fcb8d31933009effd217bed202ed2f60135b
Billy Aguinaga <aguinaga.b@gmail.com>
2025-02-14 04:04:14 -0500

    added about and services sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './navigation/header/header.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { HeroBannerComponent } from './sections/hero-banner/hero-banner.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ServicesComponent } from './sections/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, HeroBannerComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HeroBannerComponent,
+    AboutComponent,
+    ServicesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
